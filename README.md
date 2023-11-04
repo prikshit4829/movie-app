53d6dadccda1d730c4a201a9b1dd7d0a38c13f72
prikshit4829 <prikshit4829@gmail.com>
2023-11-04 16:36:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Movie App
+A movie app made in Flutter with clean architecture using BLoC, Hive and themoviedb API.
+
+## Features
+- Search feature
+- Watchlist feature
+- Now playing movies
+- Popular movies
+- Top rated movies
+- Movie details
+- Movie cast
+- Movie reviews
+- Similar movies
+- On air tv shows
+- Popular tv shows
+- Top rated tv shows
+- TV show details
+- Similar tv shows
+- TV show season details
+
+
+
+
+## Installation
+### 1. Clone the repo
+### 2. Run pub get
+```bash
+flutter pub get
+```
+### 3. Add your API key into api_constants file
+```bash
+apiKey = 'YOUR_API_KEY';
+```
+### 4. Run the app
+```bash 
+flutter run
+```
+
---
+++ lib/core/resources/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffef233c);
+  static const secondary = Color(0xff272b30);
+  static const primaryBackground = Color.fromARGB(255, 225, 225, 174);
+  static const secondaryBackground = Color.fromARGB(255, 161, 164, 124);
+  static const primaryText = Color.fromARGB(255, 0, 0, 0);
+  static const secondaryText = Color.fromARGB(255, 0, 0, 0);
+  static const primaryBtnText = Color.fromARGB(255, 0, 0, 0);
+  static const error = Colors.red;
+  static const black = Colors.black;
+  static const inactiveColor = Color(0x26ffffff);
+  static const transparent = Colors.transparent;
+  static const ratingIconColor = Color(0xffffbe21);
+  static const circleDotColor = Color(0x33ffffff);
+  static const iconContainerColor = Color.fromARGB(177, 255, 255, 255);
+}
